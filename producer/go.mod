f563c5d97a939c76c7ea95406761f78999b3d14f
Felipe Santiago <jfelipe.sfilho@gmail.com>
2020-11-12 12:43:36 -0300

    Add instrumentation to producer

--- producer/go.mod
+++ producer/go.mod
@@ -5,4 +5,7 @@ go 1.14
 require (
 	github.com/aws/aws-sdk-go v1.35.26
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/openzipkin-contrib/zipkin-go-opentracing v0.4.5
+	github.com/openzipkin/zipkin-go v0.2.5
 )
--- producer/main.go
+++ producer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,9 +12,41 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/opentracing/opentracing-go"
+	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
 )
 
+func setupGlobalTracer() {
+	// zipkin / opentracing specific stuff
+	// set up a span reporter
+	var reporterOpts []zipkinhttp.ReporterOption
+	reporterOpts = append(reporterOpts, zipkinhttp.Logger(log.New(ioutil.Discard, "", log.LstdFlags)))
+	reporter := zipkinhttp.NewReporter("http://zipkin:9411/api/v2/spans", reporterOpts...)
+
+	// create our local service endpoint
+	endpoint, err := zipkin.NewEndpoint("producer", "localhost:3000")
+	if err != nil {
+		log.Fatalf("unable to create local endpoint: %+v\n", err)
+	}
+
+	// initialize our tracer
+	nativeTracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
+	if err != nil {
+		log.Fatalf("unable to create tracer: %+v\n", err)
+	}
+
+	// use zipkin-go-opentracing to wrap our tracer
+	tracer := zipkinot.Wrap(nativeTracer)
+
+	// optionally set as Global OpenTracing tracer instance
+	log.Println(tracer)
+	opentracing.SetGlobalTracer(tracer)
+}
+
 func main() {
+	setupGlobalTracer()
 	r := chi.NewRouter()
 	svc := sqs.New(session.New(), &aws.Config{
 		Endpoint: aws.String(os.Getenv("SQS_SERVER")),
@@ -25,6 +58,8 @@ func main() {
 		)})
 	r.Use(middleware.Logger)
 	r.Get("/send", func(w http.ResponseWriter, r *http.Request) {
+		span := opentracing.StartSpan("Producer: first span")
+		defer span.Finish()
 		queueUrl := os.Getenv("SQS_SERVER") + "/queue/" + os.Getenv("QUEUE_URL")
 		params := &sqs.SendMessageInput{
 			MessageBody: aws.String("Testing 1,2,3,..."), // Required
