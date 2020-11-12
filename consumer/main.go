9ca37e0399ba90345523d31931d690df73104f6b
Felipe Santiago <jfelipe.sfilho@gmail.com>
2020-11-12 12:54:58 -0300

    Add instrumentation to consumer

--- consumer/main.go
+++ consumer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +13,40 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/opentracing/opentracing-go"
+	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
 )
 
+func setupGlobalTracer() {
+	// zipkin / opentracing specific stuff
+	// set up a span reporter
+	var reporterOpts []zipkinhttp.ReporterOption
+	reporterOpts = append(reporterOpts, zipkinhttp.Logger(log.New(ioutil.Discard, "", log.LstdFlags)))
+	reporter := zipkinhttp.NewReporter("http://zipkin:9411/api/v2/spans", reporterOpts...)
+
+	// create our local service endpoint
+	endpoint, err := zipkin.NewEndpoint("consumer", "localhost:3001")
+	if err != nil {
+		log.Fatalf("unable to create local endpoint: %+v\n", err)
+	}
+
+	// initialize our tracer
+	nativeTracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
+	if err != nil {
+		log.Fatalf("unable to create tracer: %+v\n", err)
+	}
+
+	// use zipkin-go-opentracing to wrap our tracer
+	tracer := zipkinot.Wrap(nativeTracer)
+
+	// optionally set as Global OpenTracing tracer instance
+	log.Println(tracer)
+	opentracing.SetGlobalTracer(tracer)
+}
 func main() {
+	setupGlobalTracer()
 	r := chi.NewRouter()
 	svc := sqs.New(session.New(), &aws.Config{
 		Endpoint: aws.String(os.Getenv("SQS_SERVER")),
@@ -43,6 +75,7 @@ func main() {
 				log.Println("Fail to receive message: ", err)
 			} else if len(result.Messages) > 0 {
 				for _, m := range result.Messages {
+					span := opentracing.StartSpan("Message received")
 					log.Println("Message received: ", m)
 
 					_, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
@@ -53,6 +86,7 @@ func main() {
 					if err != nil {
 						log.Println(err.Error())
 					}
+					span.Finish()
 				}
 			}
 			time.Sleep(time.Second)
